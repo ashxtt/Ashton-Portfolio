dd03bbf6f9ed6595e250c981964db97294dfd085
Ashton Bowens <ashxgrey11@gmail.com>
2023-02-23 19:46:22 -0800

    installed sass, created main & skill components

---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+
+
+export default function Main() {
+    return(
+        <div className="main">
+            <h1>Hi, I'm Ashton</h1>
+            <h1>A Software Engineer</h1>
+            <div className="Nav-button">
+                <ul className="nav-list">
+                    <button><li>Skills</li></button>
+                    <button><li>Projects</li></button>
+                    <button><li>About</li></button>
+                    <button><li>Contact</li></button>
+                </ul>
+            </div>
+        </div>
+    )
+};
---
+++ src/components/Skills.jsx
@@ -0,0 +1,17 @@
+export default function Skills(){
+
+    return(
+        <div className="skill-section">
+            <div className="title">
+                <h1>Frontend Tech</h1>
+                <h1>Other Tech</h1>
+                <h1>Backend Tech</h1>
+            </div>
+            <div className="skill-box">
+                <div className="frontend-skills"></div>
+                <div className="other-skills"></div>
+                <div className="backend-skills"></div>
+            </div>
+        </div>
+    )
+}
